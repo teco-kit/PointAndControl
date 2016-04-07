1a24492b5d90a4d371659b63cfef7ad593eeaee7
reichef <frederik.reiche@googlemail.com>
2016-04-07 15:14:24 +0200

    Added Starterclasse to start and handle an async igs task

---
+++ IGS/IGSStarter.cs
@@ -0,0 +1,57 @@
+﻿using IGS.Server.IGS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IGS
+{
+    class IGSStarter
+    {
+        public bool igsRunning { get; set; }
+        private CancellationTokenSource tokenSource { get; set; }
+        private CancellationToken igsCancellationToken { get; set; }
+        private Igs igs;
+
+
+        public IGSStarter(Igs igs)
+        {
+            igsRunning = false;
+            tokenSource = new CancellationTokenSource();
+            igsCancellationToken = tokenSource.Token;
+            this.igs = igs;
+        }
+
+
+        public async void igsStart()
+        {
+            try
+            {
+                await Task.Run(() =>
+                {
+                    igs = Initializer.InitializeIgs();
+                    igsRunning = true;
+                    while (igs.isRunning)
+                    {
+                        igsCancellationToken.ThrowIfCancellationRequested();
+                    }
+                });
+                igsRunning = false;
+            }
+            catch (OperationCanceledException e)
+            {
+                igs.shutDown();
+                igsRunning = false;
+            }
+        }
+
+        public void stopIGSConsoleCmd()
+        {
+            tokenSource.Cancel();
+
+        }
+
+    }
+}
